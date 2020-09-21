eb40fbb53ddb6301b4c18e51bf4671cb0913de6a
Tina_Q <244880572@qq.com>
2020-09-21 16:37:16 +0800

    简易计分器完善

--- app/src/main/java/com/example/androidluoex/MainActivity2.java
+++ app/src/main/java/com/example/androidluoex/MainActivity2.java
@@ -1,19 +1,19 @@
 package com.example.androidluoex;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
 
+    private final int scoreArray[]={3,2,1};
     private TextView tvResult;
     private Button btn1;          //定义按钮
     private Button btn2;
@@ -21,9 +21,8 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     private Button reset;
     private ImageButton i1;
     private ImageButton i2;
-    int cp = 0,p = 0;
+    private int Score = 0;
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +34,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         btn2 = (Button)findViewById(R.id.button2);
         btn3 = (Button)findViewById(R.id.button3);
         reset = (Button)findViewById(R.id.button4);
-        i2 = (ImageButton)findViewById(R.id.i1);
+        i1 = (ImageButton)findViewById(R.id.i1);
         i2 = (ImageButton)findViewById(R.id.i2);
 
         //设置按钮点击监听
@@ -51,19 +50,16 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.button1:
-                cp = 3;
-                outputValue(false);
+                scoreAdd(scoreArray[0]);
                 break;
             case R.id.button2:
-                cp = 2;
-                outputValue(false);
+                scoreAdd(scoreArray[1]);
                 break;
             case R.id.button3:
-                cp = 1;
-                outputValue(false);
+                scoreAdd(scoreArray[2]);
                 break;
             case R.id.button4:
-                outputValue(true);
+                reset();
                 break;
             case R.id.i1:
                 Intent intent = new Intent();
@@ -78,17 +74,17 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
             default:
         }
     }
-
-    private void outputValue(boolean is){
-        if(is){
-            p+= cp;
-            tvResult.setText(String.valueOf(p));
-        }else{
-            p = 0;
-            tvResult.setText(String.valueOf(p));
-        }
-
+    private void reset(){
+        Score=0;
+        tvResult.setText(Integer.toString(Score));
+    }
+    private void scoreAdd(int score) {
+        Score+=score;
+        ShowText();
+    }
+    private void ShowText(){
+        tvResult.setText(Integer.toString(Score));
     }
 
-}
 
+}
